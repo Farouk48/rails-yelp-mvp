f2a074b9c869d5130b966b5327d0a58d13fcd101
Farouk RAHMANI <rahmani.farouk@icloud.com>
2020-08-12 15:25:16 +0200

    views and some design done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-bouboule = Restaurant.create(name: "Bouboule", category: "french", address:"villa gaudelet", phone_number: +33 1 54 76 89 09)
-palmeraie = Restaurant.create(name: "Palmeraie", category: "belgian", address:"clermontferrand", phone_number: + 33 4 47 68 90 46)
-sushi_shop = Restaurant.create(name: "SushiShop", category: "chinese", address:"montpellier", phone_number: + 33 4 66 76 89 09)
-pasta = Restaurant.create(name: "Pasta", category: "italian", address:"couronnes", phone_number: + 33 1 56 76 43 09)
-tokyo = Restaurant.create(name: "Tokyo", category: "japanese", address:"marais", phone_number: + 33 1 54 76 65 19)
+bouboule = Restaurant.create(name: "Bouboule", category: "french", address:"villa gaudelet", phone_number: "01 54 76 89 09")
+palmeraie = Restaurant.create(name: "Palmeraie", category: "belgian", address:"clermontferrand", phone_number: "04 47 68 90 46")
+sushi_shop = Restaurant.create(name: "SushiShop", category: "chinese", address:"montpellier", phone_number: "04 66 76 89 09")
+pasta = Restaurant.create(name: "Pasta", category: "italian", address:"couronnes", phone_number: "01 56 76 43 09")
+tokyo = Restaurant.create(name: "Tokyo", category: "japanese", address:"marais", phone_number: "01 54 76 65 19")
